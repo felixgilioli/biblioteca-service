34fb9f6e894ed7bd2f7507099ab91a8fd9de1727
Felix <felix_gilioli@hotmail.com>
2023-03-15 23:30:04 -0300

    Aula 01 - Iniciando nosso projeto

---
+++ src/main/kotlin/br/com/felixgilioli/bibliotecaservice/controller/HelloController.kt
@@ -0,0 +1,13 @@
+package br.com.felixgilioli.bibliotecaservice.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+
+    @GetMapping
+    fun hello() = "Olá mundo 2"
+}
---
+++ src/test/kotlin/br/com/felixgilioli/bibliotecaservice/BibliotecaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.felixgilioli.bibliotecaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BibliotecaServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
